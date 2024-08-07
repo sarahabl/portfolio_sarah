c8c2f5397b082251ab0b030325a46540772c6886
sarahabl <sarahablonet.studio@gmail.com>
2024-08-07 10:29:21 +0200

    ✍ Màj wording Branding

--- src/pages/services/Branding.js
+++ src/pages/services/Branding.js
@@ -7,9 +7,9 @@ const Branding = () => {
   return (
     <div>
       <HeroAccueilServices 
-        primaryText="Besoin d’aide pour votre"
-        secondaryText="identité visuelle ?"
-        description="Plongez dans l'univers de l'identité visuelle, où chaque détail compte pour créer une marque qui résonne profondément avec votre public. Mon approche bienveillante et personnalisée garantit que chaque aspect de votre identité visuelle est soigneusement pensé et exécuté. Parlez-moi de votre projet et ensemble, faisons éclore une identité qui vous ressemble."
+        primaryText="Donnez vie à"
+        secondaryText="votre histoire ?"
+        description="Mon approche bienveillante et personnalisée garantit que chaque aspect de votre identité visuelle est soigneusement pensé et exécuté. Parlez-moi de votre projet et ensemble, faisons éclore une identité qui vous ressemble."
         buttonText="Voir mon portfolio"
       />
       <Bandeau />
