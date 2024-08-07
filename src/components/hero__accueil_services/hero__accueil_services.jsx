93b52375709f5950e68637fe6b6357e01b1720d3
sarahabl <sarahablonet.studio@gmail.com>
2024-08-07 09:20:01 +0200

    ✍ Ajout mots dans le bandeau

--- src/components/hero__accueil_services/hero__accueil_services.jsx
+++ src/components/hero__accueil_services/hero__accueil_services.jsx
@@ -10,12 +10,12 @@ const HeroAccueilServices = ({ primaryText, secondaryText, description, paragrap
     <div className="hero-container">
       <Container>
         <Row className="align-items-center">
-          <Col lg={6} className="hero-left">
+          <Col lg={5} className="hero-left">
             <Title primaryText={primaryText} secondaryText={secondaryText} description={description} />
             <p className="hero-paragraph">{paragraphText}</p>
             <Buttons text={buttonText} />
           </Col>
-          <Col lg={6} className="hero-right">
+          <Col lg={5} className="hero-right">
             <img src={heroImage} alt="Hero" className="hero-image" />
           </Col>
         </Row>
