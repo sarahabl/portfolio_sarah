29ee2a12216f5b40c2506f507bf179d78985206a
sarahabl <sarahablonet.studio@gmail.com>
2024-08-07 09:49:14 +0200

    ✍ mise à jour wording

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -12,7 +12,7 @@ const Intro = () => {
       </div>
       <div className="intro-right">
         <p className="description">
-        Je transforme vos idées en produits numériques efficaces et impactants. Passionnée par les design systems et le minimalisme, je combine créativité et technologie pour vos projets. Design systems harmonieux, ateliers d'innovation, sites web sur mesure : je crée des interfaces fluides et élégantes pour une expérience utilisateur optimale.
+        Je transforme vos idées en produits numériques efficaces et impactants. Passionnée par les design systems et le minimalisme, je combine créativité et technologie pour vos projets. Design systems harmonieux, ateliers de facilitation, sites web sur mesure : je crée des interfaces fluides et élégantes pour une expérience utilisateur optimale.
         </p>
       </div>
     </div>
