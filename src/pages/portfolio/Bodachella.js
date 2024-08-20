31bdbcde3b75ed449b0f90cd6323e567a18bb745
sarahabl <sarahablonet.studio@gmail.com>
2024-08-21 00:19:09 +0200

    ✨ Ajout bloc realisation sur les pages projets

--- src/pages/portfolio/Bodachella.js
+++ src/pages/portfolio/Bodachella.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Services from '../../components/services/services';
 import Contact from '../../components/contact/contact';
+import Realisations from '../../components/realisations/realisations';
 
 const Bodachella = () => {
   return (
     <div>
+      <Realisations />
       <Services />
       <Contact />
     </div>
--- src/pages/portfolio/Wenabi.js
+++ src/pages/portfolio/Wenabi.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Realisations from '../../components/realisations/realisations';
 import Services from '../../components/services/services';
 import Contact from '../../components/contact/contact';
 
 const Wenabi = () => {
   return (
     <div>
+      <Realisations />
       <Services />
       <Contact />
     </div>
