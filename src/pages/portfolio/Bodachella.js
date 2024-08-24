14d70d2d4bd26bb3533f437fe7e6061d793ed23b
sarahabl <sarahablonet.studio@gmail.com>
2024-08-24 15:45:01 +0200

    ✨ Mis à jour description projet bodachella

--- src/pages/portfolio/Bodachella.js
+++ src/pages/portfolio/Bodachella.js
@@ -12,24 +12,24 @@ import bodachellaSection2Image from '../../assets/bodachella__section2.png';
 const Bodachella = () => {
   const brandingText = `
     <strong>Recherche et Analyse</strong><br/>
-    J'ai commencé par comprendre l'**essence** de Bodachella et ses **valeurs** : fête, amitié, aventure. Cette phase a impliqué des **recherches** sur les **tendances visuelles** pour le secteur des EVJF et des **analyses** des **besoins de la marque** pour assurer une représentation **fidèle** et **attrayante**.
+    J'ai commencé par comprendre l'essence de Bodachella et ses valeurs : fête, amitié, aventure. Cette phase a impliqué des recherches sur les tendances visuelles pour le secteur des EVJF et des analyses des besoins de la marque pour assurer une représentation fidèle et attrayante.
     <br/><br/>
     <strong>Conception du Logo</strong><br/>
-    Le logo a été conçu pour être à la fois **mémorable** et **polyvalent**. J'ai choisi des **couleurs vibrantes** comme le **rose** et l'**orange** pour évoquer une ambiance **festive** et **pétillante**. La **typographie manuscrite** et **élégante** ajoute une touche **créative** et **personnelle** au design.
+    Le logo a été conçu pour être à la fois mémorable et polyvalent. J'ai choisi des couleurs vibrantes comme le rose et l'orange pour évoquer une ambiance festive et pétillante. La typographie manuscrite et élégante ajoute une touche créative et personnelle au design.
     <br/><br/>
     <strong>Déclinaisons et Applications</strong><br/>
-    Le branding a été conçu pour être **adaptable** sur divers **supports** : **coffrets cadeaux**, **réseaux sociaux**, **supports imprimés**. La **flexibilité** du design permet une **utilisation en couleurs** comme en **monochrome**, tout en maintenant une **cohérence visuelle**.
+    Le branding a été conçu pour être adaptable sur divers supports : coffrets cadeaux, réseaux sociaux, supports imprimés. La flexibilité du design permet une utilisation en couleurs comme en monochrome, tout en maintenant une cohérence visuelle.
   `;
 
   const webDevelopmentText = `
     <strong>Conception du Site</strong><br/>
-    J'ai développé un **site web** à partir de zéro, en mettant l'accent sur l’**expérience utilisateur** et l’**accessibilité**. Le site reflète l’**identité visuelle** de Bodachella tout en offrant une **interface intuitive** et **engageante**.
+    J'ai développé un site web à partir de zéro, en mettant l'accent sur l’expérience utilisateur et l’accessibilité. Le site reflète l’identité visuelle de Bodachella tout en offrant une interface intuitive et engageante.
     <br/><br/>
     <strong>Développement d’un Thème Personnalisé</strong><br/>
-    Le site utilise un **thème entièrement personnalisable**, permettant à la cliente de **modifier les contenus**, les **images** et les **sections** selon ses besoins. Cela assure une **flexibilité maximale** et une **adaptation rapide** aux **évolutions futures** de la marque.
+    Le site utilise un thème entièrement personnalisable, permettant à la cliente de modifier les contenus, les images et les sections selon ses besoins. Cela assure une flexibilité maximale et une adaptation rapide aux évolutions futures de la marque.
     <br/><br/>
     <strong>Test et Optimisation</strong><br/>
-    Avant le lancement, j’ai effectué des **tests rigoureux** pour garantir que le site fonctionne **parfaitement** sur tous les appareils. J'ai également **optimisé** le site pour la **performance** et la **vitesse**, assurant une **expérience utilisateur sans faille**.
+    Avant le lancement, j’ai effectué des tests rigoureux pour garantir que le site fonctionne parfaitement sur tous les appareils. J'ai également optimisé le site pour la performance et la vitesse, assurant une expérience utilisateur sans faille.
   `;
 
   return (
