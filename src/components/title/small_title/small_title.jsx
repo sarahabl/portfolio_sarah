72d89a3f670626a7b4589d9a58c57c4c3b522cef
sarahabl <sarahablonet.studio@gmail.com>
2024-08-25 22:41:54 +0200

    ✨ animation titre

--- src/components/title/small_title/small_title.jsx
+++ src/components/title/small_title/small_title.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import './small_title.css';
 import highlightImg from '../../../assets/Highlight.svg';
 
 const SmallTitle = ({ primaryText, secondaryText }) => {
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (titleRef.current) {
+        const scrollPosition = window.scrollY;
+        const titlePosition = titleRef.current.offsetTop;
+        const windowHeight = window.innerHeight;
+        const offset = (scrollPosition - titlePosition + windowHeight) * 0.1;
+
+        titleRef.current.style.transform = `translateY(${offset}px)`;
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="title-container">
+    <div className="title-container" ref={titleRef}>
       <div className="title-text">
         <h2 className="title-primary-small">
           {primaryText} <span className="title-secondary-small">{secondaryText}</span>
