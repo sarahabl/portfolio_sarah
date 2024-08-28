5fd85c9a5f5c10a053ae0f6fe681bb79b9f2d986
sarahabl <sarahablonet.studio@gmail.com>
2024-08-28 10:32:05 +0200

    💄 Animatio du composant services

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './services.css';
 import SmallTitle from '../title/small_title/small_title';
+import { Link } from 'react-router-dom';
 
 const services = [
   {
@@ -31,23 +32,45 @@ const services = [
 ];
 
 const Services = () => {
-  const handleServiceClick = (link) => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    window.location.href = link;
-  };
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('in-view');
+          } else {
+            entry.target.classList.remove('in-view');
+          }
+        });
+      },
+      { threshold: 0.1 } // Déclenchement à 10% de la visibilité
+    );
+
+    const elements = containerRef.current.querySelectorAll('.service-card');
+    elements.forEach((element) => observer.observe(element));
+
+    return () => {
+      elements.forEach((element) => observer.unobserve(element));
+    };
+  }, []);
 
   return (
     <section className="services-section">
       <SmallTitle primaryText="Mes" secondaryText="services" />
-      <div className="services-container">
+      <div className="services-container" ref={containerRef}>
         {services.map((service, index) => (
-          <div key={index} className="service-card" onClick={() => handleServiceClick(service.link)}>
+          <Link 
+            to={service.link} 
+            key={index} 
+            className={`service-card ${index % 2 === 0 ? 'from-left' : 'from-right'}`}
+          >
             <div className="service-info">
               <h3 className="service-title">{service.title}</h3>
               <p className="service-description">{service.description}</p>
-              <a href={service.link} className="service-link">Voir</a>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </section>
