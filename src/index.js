dd3e5051bf7e5f512d4eeb586cc8ac7bd871a6c2
sarahabl <sarahablonet.studio@gmail.com>
2024-09-02 20:45:16 +0200

    💄 Mise à jour padding

--- src/index.js
+++ src/index.js
@@ -1,44 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importation des styles Bootstrap
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
-// Pages
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import PortfolioPage from './pages/PortfolioPage';
-
-// Import des pages de services
 import Branding from './pages/services/Branding';
 import DesignSystem from './pages/services/DesignSystem';
 import Facilitation from './pages/services/Facilitation';
 import ProductDesign from './pages/services/ProductDesign';
 import SiteInternet from './pages/services/SiteInternet';
-
-// Import des pages de portfolio
 import Bodachella from './pages/portfolio/Bodachella';
 import MethodeE2R from './pages/portfolio/MethodeE2R';
 import Talia from './pages/portfolio/Talia';
 import Wenabi from './pages/portfolio/Wenabi';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <BrowserRouter>
+    <ScrollToTop />
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/portfolio" element={<PortfolioPage />} />
-        {/* Routes pour les services */}
         <Route path="/services/branding" element={<Branding />} />
         <Route path="/services/design-system" element={<DesignSystem />} />
         <Route path="/services/facilitation" element={<Facilitation />} />
         <Route path="/services/product-design" element={<ProductDesign />} />
         <Route path="/services/site-internet" element={<SiteInternet />} />
-        {/* Routes pour les projets de portfolio */}
         <Route path="/portfolio/bodachella" element={<Bodachella />} />
         <Route path="/portfolio/methode-e2r" element={<MethodeE2R />} />
         <Route path="/portfolio/talia" element={<Talia />} />
